8e25707076f9be277fe28d533d60677bed26ec15
Haaris <haaris@trainingbasket.in>
2025-03-25 13:31:11 +0530

    changed in index.jsx

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
 function Index() {
   return (
-    <div>
-      
+    <div className="border-2 border-red-500 rounded-4xl p-10 mt-2">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad nesciunt quod deleniti quas quasi aliquam laborum nostrum pariatur voluptatem. Eos?</p>
     </div>
   );
 }
