bf07d54b06140b52e32d1a604c64d83b460df354
Nashra Khan <nashrakhan@Nashras-MacBook-Air.local>
2025-03-25 13:27:22 +0530

    changed in index page

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 function Index() {
   return (
-    <div>
-      
+    <div className="bg-yellow-500">
+      <h1 className="text-2xl text-red-600">This Is Index page</h1>
     </div>
   );
 }
